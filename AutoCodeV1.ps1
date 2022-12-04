0a52c2ede8c493115019045879a56cabbbafbbd6
ComputerChicken <119811104+ComputerChicken@users.noreply.github.com>
2022-12-04 11:40:40 -0700

    Create AutoCodeV1.ps1

---
+++ AutoCodeV1.ps1
@@ -0,0 +1,21 @@
+$Password = ConvertTo-SecureString "xxxx" -AsPlainText -Force
+Write-Host "Admin Users:"
+while ( $true ) {
+    $user = Read-Host
+    if ( $user -eq "stop" ) {
+        break
+    }
+    New-LocalUser $user -Password $Password -FullName $user -Description $user -ErrorAction Ignore
+    Remove-LocalGroupMember -Group "Users" -Member $user -ErrorAction Ignore
+    Add-LocalGroupMember -Group "Administrators" -Member $user -ErrorAction Ignore 
+}
+Write-Host "Standard Users:"
+while ( $true ) {
+    $user = Read-Host
+    if ( $user -eq "stop" ) {
+        break
+    }
+    New-LocalUser $user -Password $Password -FullName $user -Description $user -ErrorAction Ignore
+    Remove-LocalGroupMember -Group "Administrators" -Member $user -ErrorAction Ignore
+    Add-LocalGroupMember -Group "Users" -Member $user -ErrorAction Ignore 
+}
