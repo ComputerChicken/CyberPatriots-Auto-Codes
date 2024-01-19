8fdfbcfac3793226087ff3587fee171f8feb3864
ComputerChicken <119811104+ComputerChicken@users.noreply.github.com>
2024-01-18 20:42:30 -0700

    Update WindowsAutoCodeV1.ps1

--- WindowsAutoCodeV1.ps1
+++ WindowsAutoCodeV1.ps1
@@ -1,4 +1,4 @@
-$Password = ConvertTo-SecureString "xxxx" -AsPlainText -Force
+$Password = ConvertTo-SecureString "securepassword" -AsPlainText -Force
 
 $authUsers = @()
 $regUsers = Invoke-Command -ScriptBlock { net user}
@@ -9,6 +9,7 @@ while ( $true ) {
     if ( $user -eq "stop" ) {
         break
     }
+    Set-LocalUser -Name $user -Password $Password -Verbose
     New-LocalUser $user -Password $Password -FullName $user -Description $user -ErrorAction Ignore
     Remove-LocalGroupMember -Group "Users" -Member $user -ErrorAction Ignore
     Add-LocalGroupMember -Group "Administrators" -Member $user -ErrorAction Ignore 
@@ -19,6 +20,7 @@ while ( $true ) {
     if ( $user -eq "stop" ) {
         break
     }
+    Set-LocalUser -Name $user -Password $Password -Verbose
     New-LocalUser $user -Password $Password -FullName $user -Description $user -ErrorAction Ignore
     Remove-LocalGroupMember -Group "Administrators" -Member $user -ErrorAction Ignore
     Add-LocalGroupMember -Group "Users" -Member $user -ErrorAction Ignore 
@@ -29,6 +31,17 @@ foreach($d in $regUsers.Split())
 {
 Write-Host $d
 	if($d -notin $authUsers) {
-		Remove-LocalUser -Name $d
+		#Remove-LocalUser -Name $d
 	}
 }
+Write-Host "Enabling Firewall..."
+Set-NetFirewallProfile -Enabled True
+Write-Host "Getting Executables..."
+$exes = (cmd.exe /c "dir .\*.exe /s") -join "`n"
+$MyInvocation.MyCommand.Path
+$dir = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
+$filedir = $dir,"executables.txt" -join "\"
+$exes | Out-File -FilePath $filedir
+Write-Host "Updating Software..."
+Update-Module
+Write-Host "Remember to do windows updates and password policies!"
